b88747cb0da21d01cf070dcc9ff2ea459d840d2c
dharmendra <dharm1180@yahoo.co.in>
2017-01-16 15:48:36 +0530

    first commit

---
+++ hello.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+public class hello
+{
+public static void main(String arg[])
+{
+System.out.println("hello hi");
+System.out.println("dharmendra thakur");
+
+}}
